ce7bea37a823c85cfc7dfadf040a5a831d0b8b76
Luis Romero <luisca990@gmail.com>
2024-12-02 19:21:21 -0500

    Se soluciona el error del ejecutable

--- Codigo/Juego/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Juego_FILE F:/Luis/Cursos/Ingeniería teleco/Tercer semes/Informatica 2/Teoria/Desafio_III/Informatica_II_Desaf_III/Codigo/Juego/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/Juego.exe)
-set(__QT_DEPLOY_TARGET_Juego_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Juego_RUNTIME_DLLS F:/instalaciones/QT_Creator/6.7.2/mingw_64/bin/Qt6Widgets.dll;F:/instalaciones/QT_Creator/6.7.2/mingw_64/bin/Qt6Gui.dll;F:/instalaciones/QT_Creator/6.7.2/mingw_64/bin/Qt6Core.dll)
